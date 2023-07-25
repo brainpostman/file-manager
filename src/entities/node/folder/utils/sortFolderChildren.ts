d6b66f42f7d63a43bca600c18c61ab41686805fa
Marat Sattarov <sattarovmk@yandex.ru>
2023-07-26 01:56:55 +0500

    Style fixes

--- src/entities/node/folder/utils/sortFolderChildren.ts
+++ src/entities/node/folder/utils/sortFolderChildren.ts
@@ -2,18 +2,18 @@ export function sortFolderChildren(childList: HTMLUListElement) {
     const childArr = Array.from(childList.children);
     childArr
         .sort((a, b) => {
-            const wrapperA = a.querySelector('article');
-            const wrapperB = b.querySelector('article');
-            if (wrapperA?.classList.contains('folder') && wrapperB?.classList.contains('file'))
-                return 1;
-            if (wrapperA?.classList.contains('file') && wrapperB?.classList.contains('folder'))
-                return -1;
-            return 0;
-        })
-        .sort((a, b) => {
             const textA = a.querySelector('.node__name')?.textContent ?? '';
             const textB = b.querySelector('.node__name')?.textContent ?? '';
             return textA.toLocaleLowerCase().localeCompare(textB.toLocaleLowerCase());
+        })
+        .sort((a, b) => {
+            const wrapperA = a.querySelector('article');
+            const wrapperB = b.querySelector('article');
+            if (wrapperA?.classList.contains('folder') && wrapperB?.classList.contains('file'))
+                return -1;
+            if (wrapperA?.classList.contains('file') && wrapperB?.classList.contains('folder'))
+                return 1;
+            return 0;
         })
         .forEach((el) => {
             childList.append(el);
